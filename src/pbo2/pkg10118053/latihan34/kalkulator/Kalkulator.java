79e6ff3d4efe1d7e61ca5047bea291b4c07c2751
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-10-17 21:36:31 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan34/kalkulator/Kalkulator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan34.kalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA     : Kevin Alvian
+ * KELAS    : IF-2
+ * NIM      : 10118053
+ */
+public class Kalkulator {
+    public double value1,value2;
+    
+    public double tambahBilangan(){
+        return value1+value2;
+    }
+    public double kurangBilangan(){
+        return value1-value2;
+    }
+    public double kaliBilangan(){
+        return value1*value2;
+    }
+    public double bagiBilangan(){
+        return value1/value2;
+    }
+    public double sisaBilagan(){
+        return value1%value2;
+    }
+    
+}
